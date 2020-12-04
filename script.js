bfe7ea80b561d81188e6f76f52b28d4a4cfdc9c6
Victor Godoy <viktorgo@gmail.com>
2020-12-03 21:46:26 -0300

    Tercer Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var respuestaC = "Software";
+var intentos = 0;
+
+function Adivina () {
+    var respuestaJ = document.getElementById('respuesta').value;
+
+    if (intentos < 4;) {
+        alert('No tiene más intentos');
+        return;
+        intentos ++;
+        if (respuestaJ == respuestaC);
+        alert('Respuesta correcta');
+        return
+    }
+
+
+    
